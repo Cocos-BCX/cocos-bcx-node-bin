f9e67aa035db4c2224a28957534d7ca8ee543df7
csquan <314588595@qq.com>
2020-02-28 18:25:47 +0800

    Update test.sh

--- fullnode/mainnet/v1.1.1/test.sh
+++ fullnode/mainnet/v1.1.1/test.sh
@@ -148,7 +148,6 @@ do_system_check() {
 
 pre_check
 init_prefix
-#do_system_check
 
 $CURL "https://raw.githubusercontent.com/Cocos-BCX/cocos-bcx-node-bin/master/fullnode/mainnet/$VERSION/config/genesis.json" -o $PREFIX/config/genesis.json
 $CURL "https://raw.githubusercontent.com/Cocos-BCX/cocos-bcx-node-bin/master/fullnode/mainnet/$VERSION/config/config.ini" -o $PREFIX/config/config.ini
